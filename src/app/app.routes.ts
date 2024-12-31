ec4bb9bee71882c4bbae414ec58d7099853b0330
Gabriel <ga.lima2205@gmail.com>
2024-12-31 11:39:39 -0300

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ContentComponent } from './pages/content/content.component';
 import { CreatePostComponent } from './pages/create-post/create-post.component';
-
+import { ContactComponent } from './pages/contact/contact.component';
 export const routes: Routes = [
     {
         path: '',
@@ -13,7 +13,15 @@ export const routes: Routes = [
         component: ContentComponent
     },
     {
-        path: 'post',
+        path: 'posts',
         component: CreatePostComponent
+    },
+    {
+        path: 'contact',
+        component: ContactComponent
+    },
+    {
+        path: '**',
+        redirectTo: '/'
     }
 ];
