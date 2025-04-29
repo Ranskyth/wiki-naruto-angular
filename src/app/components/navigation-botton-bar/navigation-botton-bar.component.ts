f0649d46dbbe0907b9f771807d91077d638314e8
gabriel <ga.lima2205@gmail.com>
2025-04-29 15:37:14 -0300

    feat : add nav button mobile

---
+++ src/app/components/navigation-botton-bar/navigation-botton-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+@Component({
+  selector: 'app-navigation-botton-bar',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './navigation-botton-bar.component.html',
+  styleUrl: './navigation-botton-bar.component.css'
+})
+export class NavigationBottonBarComponent {
+
+}
